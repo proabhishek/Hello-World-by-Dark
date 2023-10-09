8d0f85dbdda88e0e54972c6160dddafbafb55d8b
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-10-09 22:36:27 +0530

    basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// functional component in react
+// JSx - javascript + xml
+
+// 
+
+const Hello = ()=>{
+    let obj = {color:"red", textAlign:"center"}
+    let a = 100
+
+    return(
+        <div>
+            
+            <h1 className="heading">hello world</h1>
+
+
+            <h2 style={obj}> Abhishek </h2>
+            
+
+            <h3> {a} </h3>
+            <h4> {5+6+7} </h4>
+
+            <h5> {a+10+20} </h5>
+
+            <p>Hey I am doing well</p>
+        </div>
+    )
+       
+} 
+
+
+// v-17
+ReactDOM.render(<Hello /> ,  document.getElementById("root") )
+
+
+
+
+
+
+
+
+
+// function in js
+// function app(){
+//      return "hello"
+// }
+// app()
